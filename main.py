583dcf6ca99b006f026a1d32cefc43b994e2c941
Kusal Madura Yapa <101706400+kusal-madura-yapa@users.noreply.github.com>
2024-01-18 12:48:48 +0530

    Add the final app

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Hi i am succsess fully completed"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id ": item_id, "q": q}
