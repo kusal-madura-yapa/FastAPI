a4c5e0c8ba123cec0098f14372d79b52e0377e47
Kusal Madura Yapa <101706400+kusal-madura-yapa@users.noreply.github.com>
2024-01-18 15:33:46 +0530

    add some changes

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from typing import Union
+from typing import List
+from fastapi import FastAPI, HTTPException
 
-from fastapi import FastAPI
-
+# creating instence 
 app = FastAPI()
 
+items = ["apple", "banana", "orange"]
 
+
+# get the router 
 @app.get("/")
-def read_root():
-    return {"Hello": "Hi i am succsess fully completed"}
+async def root():
+   return items
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id ": item_id, "q": q}
+@app.get("/item")
+async def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/item/{item_id}")
+async def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
